e4285ffd04265e52aad09fe1bdc239d9e7626a0d
mohamedchibani <mohamedchibani1996@gmail.com>
2023-01-28 13:42:29 +0100

    added custom deploy action fix region

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -14,7 +14,7 @@ async function run() {
   // 2) upload files
   const s3Uri = `s3://${bucket}`;
 
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} -- region ${bucketRegion}`);
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   core.notice("Hello from my custom Javascript Action!");
 }
